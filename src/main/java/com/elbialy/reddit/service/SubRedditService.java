657812a099dcb1494392fba1c9e52f335b4cc39b
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-03-10 17:03:06 +0200

    Update the subreddit service to use the mapper

--- src/main/java/com/elbialy/reddit/service/SubRedditService.java
+++ src/main/java/com/elbialy/reddit/service/SubRedditService.java
@@ -1,6 +1,7 @@
 package com.elbialy.reddit.service;
 
 import com.elbialy.reddit.dto.SubRedditDto;
+import com.elbialy.reddit.mapper.SubRedditMapper;
 import com.elbialy.reddit.model.Subreddit;
 import com.elbialy.reddit.repository.SubRedditRepository;
 import jakarta.transaction.Transactional;
@@ -17,25 +18,22 @@ import java.util.stream.Collectors;
 @Slf4j
 public class SubRedditService {
     private final SubRedditRepository subRedditRepository;
+    private final SubRedditMapper subRedditMapper;
 
     @Transactional
     public SubRedditDto save(SubRedditDto subRedditDto) {
-        Subreddit subreddit = subRedditRepository.save(mapSubRedditDto(subRedditDto));
+        Subreddit subreddit = subRedditRepository.save(subRedditMapper.dtoToSubReddit(subRedditDto));
         subRedditDto.setId(subreddit.getId());
         return subRedditDto;
     }
    
     public List<SubRedditDto> getAll(){
-        return subRedditRepository.findAll().stream().map(this::mapToDto).collect(Collectors.toList());
+        return subRedditRepository.findAll().stream().map(subRedditMapper::SubRedditToDTO).collect(Collectors.toList());
 
     }
 
-    private SubRedditDto mapToDto(Subreddit subreddit) {
-        return SubRedditDto.builder().name(subreddit.getName()).description(subreddit.getDescription()).id(subreddit.getId()).build();
-    }
 
-    private Subreddit mapSubRedditDto(SubRedditDto subRedditDto) {
-        return Subreddit.builder().name(subRedditDto.getName()).description(subRedditDto.getDescription()).build();
-    }
+
+
 }
 
