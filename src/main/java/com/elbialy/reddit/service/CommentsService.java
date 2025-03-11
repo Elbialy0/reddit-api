0cd24db7403fe56264d5a9db97dcd686515a2e30
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-03-11 19:02:03 +0200

    Create comment mapper

--- src/main/java/com/elbialy/reddit/service/CommentsService.java
+++ src/main/java/com/elbialy/reddit/service/CommentsService.java
@@ -1,6 +1,11 @@
 package com.elbialy.reddit.service;
 
+import com.elbialy.reddit.dto.CommentsDto;
+import com.elbialy.reddit.model.Post;
+import com.elbialy.reddit.model.User;
 import com.elbialy.reddit.repository.CommentRepository;
+import com.elbialy.reddit.repository.PostRepository;
+import com.elbialy.reddit.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +16,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CommentsService {
     private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
     @Transactional
-    public
+    public void createComment(CommentsDto commentsDto) {
+        Post post  = postRepository.findById(commentsDto.getPostId()).orElseThrow(()->new RuntimeException("Post not found"));
+        User user = userRepository.findUserByUsername(commentsDto.getUserName()).orElseThrow(()->new RuntimeException("User not found"));
+
+    }
 }
