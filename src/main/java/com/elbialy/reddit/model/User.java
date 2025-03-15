f212325d4d4e2445a1a05ab7279489dd8503506a
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-03-16 00:31:28 +0200

    T

--- src/main/java/com/elbialy/reddit/model/User.java
+++ src/main/java/com/elbialy/reddit/model/User.java
@@ -1,9 +1,6 @@
 package com.elbialy.reddit.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,5 +23,6 @@ public class User {
     private String password;
     private String email;
     private Instant created;
+    @Column(columnDefinition = "TinyInt(1) default 0")
     private boolean enabled;
 }
